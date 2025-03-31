e332ee7c02a7134645648f90676b5ff0122f5f14
Avadhut Mane <maneavdhut26@gmail.com>
2025-03-31 18:11:04 +0530

    basic config ready

---
+++ apti-pls/src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIdA3bL3gIU4_63sSgi16ajTjtxv0ssmY",
+  authDomain: "aptiplus-3ce68.firebaseapp.com",
+  projectId: "aptiplus-3ce68",
+  storageBucket: "aptiplus-3ce68.firebasestorage.app",
+  messagingSenderId: "904444250876",
+  appId: "1:904444250876:web:92d21b63d3eeea907ae579",
+  measurementId: "G-ZV6ZH6E9BS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
